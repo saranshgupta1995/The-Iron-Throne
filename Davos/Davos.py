85b1c5e1752fac1b9a7b60647d8df87cec88a4e4
Saransh Gupta <saranshgupta1995@gmail.com>
2018-03-19 20:18:29 +0530

    Added Basic Davos

---
+++ Davos/Davos.py
@@ -0,0 +1,21 @@
+import pyttsx
+import speech_recognition as sr
+
+class Davos:
+
+    def __init__(self,citidel):
+        self.__engine = pyttsx.init()
+        self.__r = sr.Recognizer()
+        self.__citidel=citidel
+        
+    def say_this(self,sentence):
+        self.__engine.say(sentence)
+        self.__engine.runAndWait()
+
+    def listen_to_the_king(self):
+        with sr.Microphone() as source:
+            self.__r.adjust_for_ambient_noise(source, duration = 1)
+            self.say_this(self.__citidel["davos_greet"])
+            audio = self.__r.listen(source)
+            t=self.__r.recognize_google(audio)
+            return t
