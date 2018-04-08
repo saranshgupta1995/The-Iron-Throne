9b9191297dc1f14e8b3601732c6bfe1a016f9c81
Saransh Gupta <saranshgupta1995@gmail.com>
2018-04-08 11:07:02 +0530

    Mel can now open and search in chrome and calulate exps

--- Mel/Mel.py
+++ Mel/Mel.py
@@ -1,10 +1,14 @@
 import pafy
 import re, urllib, os, sys
 import urllib2
-import pyautogui
+import pyautogui, pywinauto
+
+pyautogui.MINIMUM_DURATION = 0
+pyautogui.MINIMUM_SLEEP = 0  # Default: 0.05
+pyautogui.PAUSE = 0
 
 class Mel:
-    
+
     def getYoutubeBestSearch(self,song):
         urlopen = urllib2.urlopen  # open a url
         encode = urllib.urlencode  # encode a search line
@@ -20,8 +24,33 @@ class Mel:
         print(url)
         pafy.new(url).getbestaudio(preftype="m4a").download(quiet=True)
 
-    def switchApp(self,num):
+    def switchApp(self,num=20):
         pyautogui.keyDown('alt')
         for i in range(num):
             pyautogui.press('tab')
         pyautogui.keyUp('alt')
+
+    def get_info_on(self,se='https://duckduckgo.com/?q=',query='pikachu',incog=False,goo=False):
+        if(goo):
+            se='https://google.co.in/search/?q='
+        query=query.replace(' ','+')
+        query=se+query
+        app = pywinauto.Application(backend='uia')
+        chrome_path=r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+        app.start(chrome_path + ' --force-renderer-accessibility '+('--incognito '*incog)+'--start-maximized '+query)
+
+
+
+    def calculate_this(self,exp='0'):
+        ans=0
+        print('working on '+exp)
+        try:
+            ans=eval(exp)
+        except:
+            pass
+        return str(ans)
+
+if(__name__=='__main__'):
+    m=Mel()
+
+print ('Mel loaded')
