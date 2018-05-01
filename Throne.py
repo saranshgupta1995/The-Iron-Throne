be81dce69d0c057e71c256beb5f95266353f4b96
Saransh Gupta <saranshgupta1995@gmail.com>
2018-05-02 00:55:50 +0530

    added Luwin to Throne, bran tree changes

--- Throne.py
+++ Throne.py
@@ -11,11 +11,12 @@ from Council import call_council
 global_l=threading.Lock()
 
 ## all loaded modules
-Citidel, Mel, The_Language, Tkint, Davos, LittleFinger, Bran = call_council()
+Citidel, Mel, The_Language, Tkint, Davos, LittleFinger, Bran, Luwin = call_council()
 
 ##spawn council
 citidel=Citidel.Citidel()
 mel=Mel.Mel()
+luwin=Luwin.Luwin(citidel)
 language=The_Language.Valyrian()
 face=Tkint.Face(citidel)
 davos=Davos.Davos(citidel)
@@ -103,171 +104,178 @@ def getCmdData(cmd):
 
 ## execution function
 def execCmd(action):
-    print ('gonna exec', action)
-    if(action=='raise'):
-        citidel.info_data=citidel.consts['info_raise']
-        mod=getCmdData(action)
-        kill_and_raise(mod)
-        citidel.info_data=citidel.consts['info_raise_s']
-        return 1
-    if(areSimilar("listen to the king",action)):
-        citidel.info_data=citidel.consts['info_listen_king']
-        return 1
-    if(areSimilar("get me these lyrics",action)):
-        citidel.info_data=citidel.consts['info_gmtl']
-        sleep(0.1)
-        song=pyperclip.paste()
-        link=mel.getYoutubeBestSearch(song+" lyrics")
-        download_thread=threading.Thread(target=mel.downloadAudioFrom, args=(link,))
-        download_thread.start()
-        citidel.info_data=citidel.consts['info_download_s']
-        sleep(0.1)
-        # mel.downloadAudioFrom(link)
-        return 1
-    if(areSimilar("get me this song",action)):
-        citidel.info_data=citidel.consts['info_gmts']
-        sleep(0.1)
-        song=getCmdData(action)
-        if(song):
-            link=mel.getYoutubeBestSearch(song)
+    try:
+        print ('gonna exec', action)
+        if(action=='raise'):
+            citidel.info_data=citidel.consts['info_raise']
+            mod=getCmdData(action)
+            kill_and_raise(mod)
+            citidel.info_data=citidel.consts['info_raise_s']
+            return 1
+        if(areSimilar("listen to the king",action)):
+            citidel.info_data=citidel.consts['info_listen_king']
+            return 1
+        if(areSimilar("get me these lyrics",action)):
+            citidel.info_data=citidel.consts['info_gmtl']
+            sleep(0.1)
+            song=pyperclip.paste()
+            link=mel.getYoutubeBestSearch(song+" lyrics")
             download_thread=threading.Thread(target=mel.downloadAudioFrom, args=(link,))
             download_thread.start()
             citidel.info_data=citidel.consts['info_download_s']
-            sleep(0.1)        
-##            mel.downloadAudioFrom(link)
-        return 1
-    if(action[0] in ('+','-')):
-        print('in block 1')
-        details=getCmdData(action)
-        print('got details')
-        target=getCmdData(details)
-        print(action,target,details)
-        if('salary' in details.lower()):
-            lf.add_salary(action,target)
-            return 1
-        lf.add_log(action, target, details)
-        return 1
-    elif(action[0] == '$'):
-        action=action[1:]
-        src=getCmdData('$'+action)
-        print'src',src
-        trgt=getCmdData(src)
-        print 'trgt', trgt
-        lf.do_transfer(action,src,trgt)
-        return 1
-    if(areSimilar("duck, find me this",action)):
-        query=getCmdData(action)
-        mel.get_info_on(query=query)
-        return 1
-    if(areSimilar("get the meaning of",action)):
-        print 'enter word'
-        word=getCmdData(action)
-        print ('word lookup',word)
-        word=language.get_word(word)
-        data='\n'.join(word.meanings)
-        citidel.info_data=data
-        return 1
-    if(areSimilar("tree mode",action)):
-        citidel.info_data="What do you need opened?"
-        sleep(0.1)
-        citidel.info_data="Enter Folder Name to traverse, switch mode to exit"
-        cmd=getCmdData(action)
-        get_down=False
-        path_list_needed=True
-        path_list_refresh=True
-        while(not get_down):
-            print ('top of loop',cmd)
-            if(areSimilar(cmd,'switch mode')):
-                get_down=True
-                continue
-            if(path_list_needed):
-                trgt=cmd.lower()
-                print 'choosing tree'
-                bran.choose_tree(trgt)
-                pathList=bran.tree.all_leaves
-            if(len(pathList) and path_list_refresh):
-                print 'screen data'
-                citidel.ui_scroller_data=pathList
-                citidel.ui_scroller=True
-            cmd=getCmdData(action)
-            path_list_refresh=True
-            path_list_needed=True
-            print cmd
-            if(cmd[1:] in bran.tree.file_leaves or cmd[2:] in bran.tree.file_leaves):
-                path_list_refresh=False
-                path_list_needed=False
-                continue
-            if(len(cmd)>1 and cmd[0]=='|' and cmd[1]!='.'):
-                if(bran.conflict):
-                    print 'in a conflict'
-                    bran.conflict=False
-                    path_list_needed=False
-                    bran.tree.tree=[branch for branch in cmd[1:].split('\\') if branch]
-                    pathList=bran.tree.get_all_leaves()
-                else:
-                    if(not cmd[1:] in bran.tree.file_leaves):
-                        bran.tree.climb_up(cmd[1:])
-                        pathList=bran.tree.get_all_leaves()
-                        path_list_needed=False
-
-            if(cmd[:2]=='>>'):
-                if(not cmd[2:] in bran.tree.file_leaves):
-                    bran.get_inside(cmd[2:])
-                    path_list_needed=False
-                    path_list_refresh=False
-            if(cmd[1:3]=='..'):
-                print('tree', bran.tree.tree)
-                if(len(bran.tree.tree)>1):
-                    bran.tree.move_down_branch()
-                    pathList=bran.tree.get_all_leaves()
-                path_list_needed=False
-
-        citidel.ui_scroller=True
-        citidel.ui_scroller_data=[]
-
-    if(areSimilar("open this",action)):
-        citidel.info_data="What do you need opened?"
-        trgt=getCmdData(action)
-        if(len(bran.show_glimpse(trgt))==1):
-            bran.get_inside(bran.get_path(trgt))
-    if(areSimilar("get word data",action)):
-        print 'enter word'
-        word=getCmdData(action)
-        print ('word lookup',word)
-        word=language.get_word(word)
-        data_m='\n'.join(word.meanings)
-        data_sy='\n'.join(word.synonyms)
-        data_sn='\n'.join(word.sentences)
-        citidel.info_data='It may mean any of the following.'
-        sleep(0.1)
-        citidel.info_data=data_m
-        sleep(0.1)
-        citidel.info_data='Do you need the synonyms? Enter more for more info.'
-        need=getCmdData(word.word)
-        if(need=='more'):
-            citidel.info_data=data_sy
             sleep(0.1)
-            citidel.info_data='Do you need some example sentences? Enter more for more info.'
-            need=getCmdData(need)
+            # mel.downloadAudioFrom(link)
+            return 1
+        if(areSimilar("get me this song",action)):
+            citidel.info_data=citidel.consts['info_gmts']
+            sleep(0.1)
+            song=getCmdData(action)
+            if(song):
+                link=mel.getYoutubeBestSearch(song)
+                download_thread=threading.Thread(target=mel.downloadAudioFrom, args=(link,))
+                download_thread.start()
+                citidel.info_data=citidel.consts['info_download_s']
+                sleep(0.1)
+            return 1
+        if(action[0] in ('+','-')):
+            details=getCmdData(action)
+            target=getCmdData(details)
+            if('salary' in details.lower()):
+                lf.add_salary(action,target)
+                return 1
+            lf.add_log(action, target, details)
+            return 1
+        elif(action[0] == '$'):
+            luwin.new_event('money transfer')
+            action=action[1:]
+            src=getCmdData('$'+action)
+            luwin.in_event(action, src)
+            trgt=getCmdData(src)
+            luwin.in_event(action, src, trgt)
+            lf.do_transfer(action,src,trgt)
+            return 1
+        if(areSimilar("duck, find me this",action)):
+            luwin.new_event('ducky search')
+            query=getCmdData(action)
+            luwin.in_event(query)
+            mel.get_info_on(query=query)
+            luwin.end_event()
+            return 1
+        if(areSimilar("get the meaning of",action)):
+            luwin.new_event('meaning fetch')
+            word=getCmdData(action)
+            luwin.in_event(word)
+            word=language.get_word(word)
+            data='\n'.join(word.meanings)
+            ## TODO check for empty data
+            citidel.info_data=data
+            sleep(0.1)
+            luwin.end_event()
+            return 1
+        if(areSimilar("tree mode",action)):
+            citidel.info_data="What do you need opened?"
+            sleep(0.1)
+            citidel.info_data="Enter Folder Name to traverse, switch mode to exit"
+            cmd=getCmdData(action)
+            get_down=False
+            path_list_needed=True
+            path_list_refresh=True
+            while(not get_down):
+                print ('top of loop',cmd)
+                if(areSimilar(cmd,'switch mode')):
+                    get_down=True
+                    continue
+                if(path_list_needed):
+                    trgt=cmd.lower()
+                    print 'choosing tree'
+                    bran.choose_tree(trgt)
+                    pathList=bran.tree.all_leaves
+                if(len(pathList) and path_list_refresh):
+                    print 'screen data'
+                    citidel.ui_scroller_data=pathList
+                    citidel.ui_scroller=True
+                cmd=getCmdData(action)
+                path_list_refresh=True
+                path_list_needed=True
+                print cmd
+                if(cmd[1:] in bran.tree.file_leaves or cmd[2:] in bran.tree.file_leaves):
+                    path_list_refresh=False
+                    path_list_needed=False
+                    continue
+                if(len(cmd)>1 and cmd[0]=='|' and cmd[1]!='.'):
+                    if(bran.conflict):
+                        print 'in a conflict'
+                        bran.conflict=False
+                        path_list_needed=False
+                        bran.tree.tree=[branch for branch in cmd[1:].split('\\') if branch]
+                        pathList=bran.tree.get_all_leaves()
+                    else:
+                        if(not cmd[1:] in bran.tree.file_leaves):
+                            bran.tree.climb_up(cmd[1:])
+                            pathList=bran.tree.get_all_leaves()
+                            path_list_needed=False
+
+                if(cmd[:2]=='>>'):
+                    if(not cmd[2:] in bran.tree.file_leaves):
+                        bran.get_inside(cmd[2:])
+                        path_list_needed=False
+                        path_list_refresh=False
+                if(cmd[1:3]=='..'):
+                    print('tree', bran.tree.tree)
+                    if(len(bran.tree.tree)>1):
+                        bran.tree.move_down_branch()
+                        pathList=bran.tree.get_all_leaves()
+                    path_list_needed=False
+
+            citidel.ui_scroller=True
+            citidel.ui_scroller_data=[]
+
+        if(areSimilar("open this",action)):
+            citidel.info_data="What do you need opened?"
+            trgt=getCmdData(action)
+            if(len(bran.show_glimpse(trgt))==1):
+                bran.get_inside(bran.get_path(trgt))
+        if(areSimilar("get word data",action)):
+            print 'enter word'
+            word=getCmdData(action)
+            print ('word lookup',word)
+            word=language.get_word(word)
+            data_m='\n'.join(word.meanings)
+            data_sy='\n'.join(word.synonyms)
+            data_sn='\n'.join(word.sentences)
+            citidel.info_data='It may mean any of the following.'
+            sleep(0.1)
+            citidel.info_data=data_m
+            sleep(0.1)
+            citidel.info_data='Do you need the synonyms? Enter more for more info.'
+            need=getCmdData(word.word)
             if(need=='more'):
-                citidel.info_data=data_sn
-        return 1
-    if(areSimilar("duck, work secretly",action)):
-        query=getCmdData(action)
-        mel.get_info_on(query=query,incog=True)
-        return 1
-    if(areSimilar("goo, find me this",action)):
-        query=getCmdData(action)
-        mel.get_info_on(query=query,goo=True)
-        return 1
-    if(areSimilar("goo, work secretly",action)):
-        query=getCmdData(action)
-        mel.get_info_on(query=query,goo=True,incog=True)
-        return 1
-    if(areSimilar("calculate exp",action)):
-        pyperclip.copy(mel.calculate_this(pyperclip.paste()))
-        return 1
-    return 0
+                citidel.info_data=data_sy
+                sleep(0.1)
+                citidel.info_data='Do you need some example sentences? Enter more for more info.'
+                need=getCmdData(need)
+                if(need=='more'):
+                    citidel.info_data=data_sn
+            return 1
+        if(areSimilar("duck, work secretly",action)):
+            query=getCmdData(action)
+            mel.get_info_on(query=query,incog=True)
+            return 1
+        if(areSimilar("goo, find me this",action)):
+            query=getCmdData(action)
+            mel.get_info_on(query=query,goo=True)
+            return 1
+        if(areSimilar("goo, work secretly",action)):
+            query=getCmdData(action)
+            mel.get_info_on(query=query,goo=True,incog=True)
+            return 1
+        if(areSimilar("calculate exp",action)):
+            pyperclip.copy(mel.calculate_this(pyperclip.paste()))
+            return 1
+        return 0
+    except Exception as e:
+        luwin.log_err(e)
+        sleep(0.1)
 
 def show_ui():
     while True:
