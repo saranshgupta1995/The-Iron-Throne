92ce21b4af22b4d014e61f53a2a2310f9d4894b6
Saransh Gupta <saranshgupta1995@gmail.com>
2018-05-30 22:37:10 +0530

    added setup.bat

---
+++ setup.bat
@@ -0,0 +1,18 @@
+c:
+
+cd Python27
+cd Scripts
+
+pip install pygame
+pip install pyautogui
+pip install pywinauto
+pip install pyttsx
+pip install SpeechRecognition
+pip install requests
+pip install BeautifulSoup
+pip install openpyxl
+pip install pyperclip
+pip install pafy
+pip install nltk
+pip install pyHook
+pip install logging
