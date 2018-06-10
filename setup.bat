344eda2cb527783f06b9f8332fbba50cc194be83
Saransh Gupta <saranshgupta1995@gmail.com>
2018-06-10 22:23:12 +0530

    added pyhook 32-bit version

--- setup.bat
+++ setup.bat
@@ -3,6 +3,7 @@ python -m pip install -r requirements.txt
 cd Module
 
 python -m pip install pyHook-1.5.1-cp27-cp27m-win_amd64.whl
+python -m pip install pyHook-1.5.1-cp27-cp27m-win32.whl
 
 cd ..
 
